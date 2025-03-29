d2f02b58c2620d49c6825fdef2cc00b06c0990a9
Mahmoud Elmasry <me653830@gmail.com>
2025-03-30 00:12:06 +0200

    added products slider html+css only

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-// callings
-
-// swipper library
-
-var swiper = new Swiper(".slide-swp", {
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-  },
-  loop: true,
-});
-// -----------------------
+// ----------------------
 
 let categList = document.querySelector(".category-nav-list");
 console.log(categList);
---
+++ js/swiper.js
@@ -0,0 +1,29 @@
+// callings
+
+// swipper library
+
+var swiper = new Swiper(".slide-swp", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".slide-product", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+  },
+  loop: true,
+});
