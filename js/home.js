f15ec76e2cce35a1fe23a5545438ab849f32b366
Mahmoud Elmasry <me653830@gmail.com>
2025-04-01 21:31:42 +0200

    finshed cart html,css

--- js/home.js
+++ js/home.js
@@ -188,3 +188,14 @@ fetch("products.json")
       }
     });
   });
+
+// cart
+window.onload(() => {
+  let cart = document.querySelector(".cart");
+  cart.style.marginRight = "-350px";
+});
+function showHideCart() {
+  let cart = document.querySelector(".cart");
+  cart.classList.toggle("actv");
+}
+showHideCart();
