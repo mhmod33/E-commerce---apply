7093f0f9fd0eee7636bc0c2f870ccfdf8af665ba
Mahmoud Elmasry <me653830@gmail.com>
2025-04-05 00:11:30 +0200

    finished checkout js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@ function categlist() {
   }
 
 
+  function showHideCart() {
+    let cart = document.querySelector(".cart");
+    cart.classList.toggle("actv");
+  }
+  showHideCart();
+
 // *********************************------------------add to cart functions --------------------*********************************
 
 fetch("products.json")
@@ -23,6 +29,7 @@ fetch("products.json")
       button.addEventListener("click", (event) => {
         const productId = event.target.getAttribute("data-id");
         let selectedItem = data.find((product) => product.id == productId);
+
 
         let allMatchingItems = document.querySelectorAll(
           `.btn-add-to-cart[data-id="${productId}"]`
@@ -39,7 +46,6 @@ fetch("products.json")
   });
 
 function addToCartFun(product) {
-  console.log(product);
   const cartItem = JSON.parse(localStorage.getItem("CartItem")) || [];
   cartItem.push({ ...product, quantity: 1 });
   localStorage.setItem("CartItem", JSON.stringify(cartItem));
@@ -52,10 +58,15 @@ function updateCart() {
   const cartItem = JSON.parse(localStorage.getItem("CartItem")) || [];
   const itemsInCart = document.getElementById("cart-items");
 
+  let cartInCheckoutItem = document.getElementById('checkoutItems');
+    if(cartInCheckoutItem){ 
+      cartInCheckoutItem.innerHTML = "";  
+    }
 
   let totalPrice=0;
   let totalCount=0;
 
+ 
   itemsInCart.innerHTML = "";
   cartItem.forEach((item, index) => {
 
@@ -75,11 +86,29 @@ function updateCart() {
     totalPrice+=totalPriceForItem;
     let totalPriceCalc=document.querySelector('.price-cart-total');
     totalPriceCalc.textContent=`$${totalPrice}`;
+    
+    // calc total price for all items in checkout 
 
 
 
+    if(cartInCheckoutItem){ 
+      let subtotalIncheckout=document.querySelector('.subtotal-checkout');
+      subtotalIncheckout.innerHTML=`$${totalPriceForItem}`;
+      let shippingTotal=document.querySelector('.shippinCheckout');
+      let totalCheckout=document.querySelector('.total-checkout');
+  
+  
+      let SubTotalAsNumber=Number(subtotalIncheckout.textContent.replace('$',''));
+      let shippingTotalAsNumber=Number(shippingTotal.textContent.replace('$',''));
+      totalCheckout.textContent=`$${SubTotalAsNumber+shippingTotalAsNumber}`;
+    }
+
+    
+
+  
+  
     itemsInCart.innerHTML += `
-       <div class="item-cart">
+      <div class="item-cart">
                 <img src="${item.img}" alt="">
                 <div class="item-content">
                     <h4>${item.name}</h4>
@@ -91,11 +120,40 @@ function updateCart() {
                     </div>
                 </div>
                 <button class="delete-item" data-index=${item.index} ><i class="fa-solid fa-trash-can"></i></button>
-            </div>`;
+      </div>`;
+
+
+    if(cartInCheckoutItem){ 
+      cartInCheckoutItem.innerHTML+=`  
+      <div class="item_cart">
+        <div class="image_name">
+            <img src="${item.img}" alt="">
+
+            <div class="content">
+                <h4>${item.name}</h4>
+                <p class="price-cart">$${totalPriceForItem}</p>
+
+                <div class="quantity_control">
+                <button class="decrease-quantity" data-index=${index}>-</button>
+                <span class="quantity">${item.quantity}</span>
+                <button class="increase-quantity" data-index=${index}>+</button>
+                </div>
+
+            </div>
+        </div>
+    
+
+        <button class="delete-item" data-index=${item.index}><i class="fa-solid fa-trash-can"></i></button>
+
+      </div>`;}
+
+
+           
   })
 let increaseBtn=document.querySelectorAll('.increase-quantity');
 let decreaseBtn=document.querySelectorAll('.decrease-quantity');
 
+  ////////////////////////////////////////////////// increase item ////////////////////////////////////////////////
 
 increaseBtn.forEach(button=>{
   button.addEventListener('click',(event)=>{
@@ -105,6 +163,7 @@ increaseBtn.forEach(button=>{
 });
 
 
+  ////////////////////////////////////////////////// Decrease item ////////////////////////////////////////////////
 
 decreaseBtn.forEach(button=>{
   button.addEventListener('click',(event)=>{
@@ -118,13 +177,16 @@ decreaseBtn.forEach(button=>{
 
   ////////////////////////////////////////////////// delete item and remove it from  cart ////////////////////////////////////////////////
   let deleteButton = document.querySelectorAll(".delete-item");
-  deleteButton.forEach((button) => {
+    deleteButton.forEach((button) => {
     button.addEventListener("click", (event) => {
       let itemIndex = event.target.closest("button").getAttribute("data-index");
       removeItem(itemIndex);
     });
   });
+
 }
+
+
 
 
 
@@ -133,9 +195,11 @@ function removeItem(index) {
   let removeProduct = cartItem.splice(index, 1)[0];
   localStorage.setItem("CartItem", JSON.stringify(cartItem));
   updateCart();
-
   updateButtonsSatate(removeProduct.id);
 }
+
+
+
 ////////////////////// update the status of the function, that when the item removed from the cart it returns to the ordinary status ////////////////////////////////////////////////
 
 function updateButtonsSatate(productId) {
